c0597d7f1cedbb25c87798918dc581446b46b069
julafrt <151755167+julafrt@users.noreply.github.com>
2024-04-11 10:55:25 +0200

    Add files via upload

--- project.py
+++ project.py
@@ -78,7 +78,7 @@ def plot_top_songs(spotify_filtered, top_range=(0, 25), x_axis='energy_%', y_axi
                     domain=['in_spotify_playlists', 'in_apple_playlists', 'in_deezer_playlists'],
                     range=['darkgreen', 'crimson', 'MediumOrchid']
                 ),
-                title='Metrics', legend=alt.Legend(padding=-50)
+                title='Metrics', legend=alt.Legend(legendX=500)
             ), alt.value('lightgray')
         )
     ).properties(
